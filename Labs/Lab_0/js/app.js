026ec3ae40b4e51afea353c2598cdf30c7993511
Brendan6374 <tyner100@gmail.com>
2023-01-12 12:53:44 -0500

    Adding Base Lab File

---
+++ Labs/Lab_0/js/app.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(800, 600);
+    background(35, 117, 158);
+
+
+    fill(252, 45, 187);
+    rect( 40,50, 100, 20);
+}
+
+
+function draw() {
+    circle(mouseX, mouseY, 12)
+}
