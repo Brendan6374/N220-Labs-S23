fba7abf8597c1f20d0518242a34d95c8ccb420b5
Brendan6374 <tyner100@gmail.com>
2023-02-16 17:15:19 -0500

    hw 5
    I still couldnt figure out 2 and 1. But I know it's close

--- hw/hw5/one/appFiveOne.js
+++ hw/hw5/one/appFiveOne.js
@@ -1,5 +1,26 @@
 //create square div in HTML//
+let divSquare = document.getElementById("square")
 
+
+let square = {
+    x: 100,
+    y: 100,    
+}
+
+
+console.log("square")
+
+divSquare.style.width = square.x + "px"
+divSquare.style.height = square.y + "px" 
+divSquare.style.backgroundColor = "#00ff00"
+
+
+square.onclick = function() {
+    divSquare.style.width = square.x * 1.1 + "px"
+    divSquare.style.height = square.y * 1.1 +"px"   
+
+
+}
 /// size square in java///
 ///Sqaure will more then likely have to be object with variable height///
 
--- hw/hw5/three/appFiveThree.js
+++ hw/hw5/three/appFiveThree.js
@@ -1,3 +1,21 @@
 //create  blue square div
+let divSquare = document.getElementById("square")
+
+divSquare.style.height = 100 + "px"
+divSquare.style.width = 100 + "px"
+divSquare.style.backgroundColor = "#0000ff"
+
+square.onmouseover = function backgroundColor() {
+
+divSquare.style.backgroundColor = "#000000"
+
+} 
+
+square.onmouseout = function backgroundColor() {
+
+    divSquare.style.backgroundColor = "#0000ff"
+    
+    } 
+
 
 //use onmouseover commands instead of onclick commands to chage over the div
